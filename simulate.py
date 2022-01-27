1c12ad46d7bdabdf208fd8b42015ee4cf5a09820
noeldesmarais <npd29@comcast.net>
2022-01-27 18:34:13 -0500

    added generate.py

--- simulate.py
+++ simulate.py
@@ -1,9 +1,9 @@
 import pybullet as p
 import time
 physicsClient = p.connect(p.GUI)
+p.loadSDF("box.sdf")
 for i in range(1000):
     p.stepSimulation()
     time.sleep(.001)
     print(i)
-
 p.disconnect()
